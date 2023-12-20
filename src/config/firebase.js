56dc0c877b2eafd519e43a67a22b840bd184061d
Chani Viner <72949114+ChanZil@users.noreply.github.com>
2023-12-20 23:04:55 +0200

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,13 +4,7 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBzyNF5U8MF_0nTEuCRKwcS3H077fdGIdU",
-    authDomain: "resumebuilder-93c12.firebaseapp.com",
-    projectId: "resumebuilder-93c12",
-    storageBucket: "resumebuilder-93c12.appspot.com",
-    messagingSenderId: "489041093785",
-    appId: "1:489041093785:web:b1112dc32e5ba376014408",
-    measurementId: "G-LNTE0631VJ"
+    //hidden
 };
 
 // Initialize Firebase
